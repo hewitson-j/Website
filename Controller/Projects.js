2619491b4e196814db97bcf89df0d38d3738c1ae
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2022-12-08 09:48:38 -0700

    Formatting for Projects, added accordion.

---
+++ Controller/Projects.js
@@ -0,0 +1,22 @@
+// const accordion = document.querySelector(".accordion-container");
+
+// accordion.addEventListener("click", (event) => {
+//   const clickedElement = event.target;
+
+//   if ([...clickedElement.classList].includes("accordion-head")) {
+//     clickedElement.nextElementSibling;
+
+//     sibling.classList.toggle("accordion-body-open");
+//   }
+// });
+
+// const accordion = document.getElementsByClassName("accordion-head");
+
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//   });
+// }
+
+const body = document.body;
+body.append("Bonjour stupid.");
